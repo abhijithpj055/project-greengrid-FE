19065b985e6f6be06638660d5a7c65f61d87bedf
Abhijith P J <apj42211@gmai.com>
2024-03-30 10:24:03 +0530

    design

--- src/Components/AuthBox/SignUpBox.jsx
+++ src/Components/AuthBox/SignUpBox.jsx
@@ -1,27 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './LoginBox.css'
 import CustomInput from '../Common/CustomInput/CustomInput'
 
 function SignUpBox({setBoxType}) {
+  const [signupData,setSignupData]=useState({})
+  const handleChange=(e)=>{
+    setSignupData({...signupData,[e.target.name]:e.target.value})
+  }
+  
+  const doSignup=()=>{
+
+  }
+
   return (
    <><div className='d-flex flex-column'>
      <div className='mt-4'>
-              <CustomInput label={'Name'} value={null}/>
+              <CustomInput label={'First Name'} name={'firstName'} value={signupData.firstName} onchange={handleChange}/>
      </div>
      <div className='mt-4'>
-              <CustomInput label={'Email'} value={null}/>
+              <CustomInput label={'Last Name'} name={'lastName'} value={signupData.lastName} onchange={handleChange}/>
      </div>
      <div className='mt-4'>
-              <CustomInput label={'Mobile Number'} value={null}/>
+              <CustomInput label={'Email'} name={'email'} value={signupData.email} onchange={handleChange}/>
      </div>
      <div className='mt-4'>
-              <CustomInput label={'Password'} value={null}/>
+              <CustomInput label={'Mobile Number'} name={'mobileNumber'} value={signupData.mobileNumber} onchange={handleChange}/>
      </div>
      <div className='mt-4'>
-              <CustomInput label={'Confirm Password'} value={null}/>
+              <CustomInput label={'Password'} name={'password'} value={signupData.password} onchange={handleChange}/>
+     </div>
+     <div className='mt-4'>
+              <CustomInput label={'Confirm Password'} name={'confirmPassword'} value={signupData.confirmPassword} onchange={handleChange}/>
      </div>
 
-     <button className='common-button mt-4 align-self-center'>Sign up</button>
+     <button className='common-button mt-4 align-self-center' onClick={doSignup}>Sign up</button>
 
      <p className='already-account mt-4'>already have an account<i onClick={()=>setBoxType('login')}> Login here</i></p>
 
