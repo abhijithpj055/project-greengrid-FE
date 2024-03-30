8afab095f70dc045179d79230615c2087990353c
Abhijith P J <apj42211@gmai.com>
2024-03-30 23:10:14 +0530

    chng fe

--- src/Components/AuthBox/LoginBox.jsx
+++ src/Components/AuthBox/LoginBox.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginBox.css'
 import CustomInput from '../Common/CustomInput/CustomInput'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { errorToast, successToast } from '../../Plugins/Toast/Toast'
 
 function LoginBox({setBoxType}) {
+  const [loginData,setLoginData]=useState({})
+  const navigate=useNavigate()
+
+  const handleLogin=(e)=>{
+    setLoginData({...loginData,[e.target.name]:e.target.value})
+  }
+
+   const doLogin=()=>{
+     axios({
+      method:'POST',
+      url:process.env.REACT_APP_BASE_URL+'/auth/dologin',
+      data:loginData
+    }).then((res)=>{
+
+      localStorage.setItem('token',res.data.token)
+      successToast(res.message)
+       navigate('/home')
+
+    })
+     .catch((err)=>{
+       errorToast(err?.response?.data.message || 'something went to wrong')
+
+    })
+
+ }
+
+  
+
   return (
    <> <div className='d-flex flex-column'>
            <div className='mt4'>
-              <CustomInput label={'Email'} value={null}/>
+              <CustomInput label={'Email'} value={loginData.email} name={'email'} onchange={handleLogin}/>
             </div>
             <div className='mt-4'>
-              <CustomInput label={'password'} value={null}/>
+              <CustomInput label={'password'} value={loginData.password} name={'password'} onchange={handleLogin}/>
            </div>
     
-           <button className='common-button mt-4 align-self-center'>Login</button>
+           <button className='common-button mt-4 align-self-center' onClick={doLogin}>Login</button>
 
            <p className='already-account mt-4'>Dont have an account<i onClick={()=>setBoxType('signup')}> sign up here</i></p>
 
--- src/Components/AuthBox/SignUpBox.jsx
+++ src/Components/AuthBox/SignUpBox.jsx
@@ -1,14 +1,36 @@
 import React, {useState} from 'react'
 import './LoginBox.css'
 import CustomInput from '../Common/CustomInput/CustomInput'
+import { errorToast, successToast } from '../../Plugins/Toast/Toast'
+import axios from 'axios'
 
 function SignUpBox({setBoxType}) {
   const [signupData,setSignupData]=useState({})
   const handleChange=(e)=>{
     setSignupData({...signupData,[e.target.name]:e.target.value})
   }
-  
+
   const doSignup=()=>{
+    if(signupData.password===signupData.confirmPassword){
+
+      axios({
+        method:'POST',
+        url:process.env.REACT_APP_BASE_URL+'/auth/dosignup',
+        data:signupData
+      }).then((res)=>{
+        successToast(res.message)
+        setBoxType('login')
+
+      })
+      .catch((err)=>{
+        errorToast(err?.response?.data.message || 'something went to wrong')
+
+      })
+
+    }else{
+
+      errorToast('Enter the same password')
+    }
 
   }
 
@@ -21,16 +43,16 @@ function SignUpBox({setBoxType}) {
               <CustomInput label={'Last Name'} name={'lastName'} value={signupData.lastName} onchange={handleChange}/>
      </div>
      <div className='mt-4'>
-              <CustomInput label={'Email'} name={'email'} value={signupData.email} onchange={handleChange}/>
+              <CustomInput type={'email'} label={'Email'} name={'email'} value={signupData.email} onchange={handleChange}/>
      </div>
      <div className='mt-4'>
-              <CustomInput label={'Mobile Number'} name={'mobileNumber'} value={signupData.mobileNumber} onchange={handleChange}/>
+              <CustomInput type={'number'} label={'Mobile Number'} name={'mobileNumber'} value={signupData.mobileNumber} onchange={handleChange}/>
      </div>
      <div className='mt-4'>
-              <CustomInput label={'Password'} name={'password'} value={signupData.password} onchange={handleChange}/>
+              <CustomInput type={'password'} label={'Password'} name={'password'} value={signupData.password} onchange={handleChange}/>
      </div>
      <div className='mt-4'>
-              <CustomInput label={'Confirm Password'} name={'confirmPassword'} value={signupData.confirmPassword} onchange={handleChange}/>
+              <CustomInput type={'password'} label={'Confirm Password'} name={'confirmPassword'} value={signupData.confirmPassword} onchange={handleChange}/>
      </div>
 
      <button className='common-button mt-4 align-self-center' onClick={doSignup}>Sign up</button>
